b274a6472e3ea64a96a45825cda7eb887c5c78cc
Alexander Jackson <alexanderjackson@protonmail.com>
2020-11-01 09:33:58 +0000

    Move the code to src/lib.rs and document
    Move the codebase to `src/lib.rs` to allow for easier doctesting and
    documentation support, and write documentation for each method.

---
+++ src/lib.rs
@@ -0,0 +1,197 @@
+#![feature(decl_macro)]
+#![warn(clippy::all)]
+#![warn(clippy::pedantic)]
+
+use std::env;
+
+use dotenv::dotenv;
+use oauth::{Builder, Credentials};
+use reqwest::blocking::Client;
+use rocket::http::RawStr;
+use rocket::response::Redirect;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(oauth::Request)]
+struct Request {
+    scope: &'static str,
+    expiry: &'static str,
+}
+
+#[derive(oauth::Request)]
+struct Access<'a> {
+    oauth_token: &'a str,
+}
+
+const SCOPE: &str = "urn:websignon.warwick.ac.uk:sso:service";
+const EXPIRY: &str = "forever";
+const OAUTH_CALLBACK: &str = "http://localhost:8000/authorised";
+
+const REQUEST_TOKEN_URL: &str = "https://websignon.warwick.ac.uk/oauth/requestToken";
+const AUTHORISE_TOKEN_URL: &str = "https://websignon.warwick.ac.uk/oauth/authorise";
+const ACCESS_TOKEN_URL: &str = "https://websignon.warwick.ac.uk/oauth/accessToken";
+
+/// Splits a token of the form `x=y` and returns the `y` portion.
+///
+/// # Examples
+///
+/// ```
+/// # use rocket_oauth::parse_token;
+/// let token = "token=abcde";
+/// let parsed = parse_token(token);
+///
+/// assert_eq!(parsed, "abcde");
+/// ```
+pub fn parse_token(token: &str) -> &str {
+    let index = token.find('=').unwrap();
+    &token.split_at(index).1[1..]
+}
+
+/// Splits tokens of the form `a=b&c=d` and returns the `b` and `d` portions.
+///
+/// # Examples
+///
+/// ```
+/// # use rocket_oauth::parse_tokens;
+/// let stream = "token=abcde&secret=other";
+/// let (token, secret) = parse_tokens(stream);
+///
+/// assert_eq!(token, "abcde");
+/// assert_eq!(secret, "other");
+/// ```
+pub fn parse_tokens(request_token: &str) -> (&str, &str) {
+    let tokens: Vec<&str> = request_token.split('&').collect();
+    (parse_token(tokens[0]), parse_token(tokens[1]))
+}
+
+/// Represents the root of the website. Users are immediately redirected to sign in.
+///
+/// Gets the `CONSUMER_KEY` and `CONSUMER_SECRET` values from the environment and requests a new
+/// token from the SSO provider. The secret is then written to disk currently, and the token is
+/// used to form a redirect, which is returned to the user so they can sign in through the
+/// provider.
+#[get("/")]
+fn index() -> Redirect {
+    let consumer_key = env::var("CONSUMER_KEY").unwrap();
+    let consumer_secret = env::var("CONSUMER_SECRET").unwrap();
+
+    let request_token = obtain_request_token(&consumer_key, &consumer_secret);
+    log::info!("Request token obtained: {}", request_token);
+
+    let (token, secret) = parse_tokens(&request_token);
+
+    std::fs::write("secret.txt", secret).unwrap();
+
+    let url = format!(
+        "{}?oauth_token={}&oauth_callback={}",
+        AUTHORISE_TOKEN_URL, token, OAUTH_CALLBACK
+    );
+
+    Redirect::to(url)
+}
+
+/// Represents the callback of the website. Users are sent here after signing in through SSO.
+///
+/// Gets the parameters from the query string and logs them to the terminal before requesting to
+/// exchange the request token for an access token. If this succeeds, logs the token and displays
+/// it on the frontend to the user.
+#[get("/authorised?<oauth_token>&<user_id>&<oauth_verifier>")]
+fn authorised(oauth_token: &RawStr, user_id: &RawStr, oauth_verifier: &RawStr) -> String {
+    let request_token = oauth_token.as_str();
+    let user_id = user_id.as_str();
+    let oauth_verifier = oauth_verifier.as_str();
+
+    log::info!("Request token authorized: {}", request_token);
+    log::info!("User ID: {}", user_id);
+    log::info!("OAuth Verifier: {}", oauth_verifier);
+
+    let consumer_key = env::var("CONSUMER_KEY").unwrap();
+    let consumer_secret = env::var("CONSUMER_SECRET").unwrap();
+    let access_token = exchange_request_for_access(
+        &consumer_key,
+        &consumer_secret,
+        request_token,
+        oauth_verifier,
+    );
+
+    log::info!("Access token received: {}", access_token);
+
+    access_token
+}
+
+/// Obtains a request token from the OAuth provider, corresponding to Stage 1.
+///
+/// Using the `consumer_key` and `consumer_secret`, signs a request to the SSO service and requests
+/// a new request token. This can then be used later on to become an access token.
+fn obtain_request_token(consumer_key: &str, consumer_secret: &str) -> String {
+    let credentials = Credentials::new(consumer_key, consumer_secret);
+    let request = Request {
+        scope: SCOPE,
+        expiry: EXPIRY,
+    };
+    let auth = Builder::<_, _>::new(credentials, oauth::HmacSha1)
+        .callback(OAUTH_CALLBACK)
+        .post(&REQUEST_TOKEN_URL, &request);
+
+    let client = Client::new();
+    let request = client
+        .post(REQUEST_TOKEN_URL)
+        .header("Authorization", auth)
+        .header("User-Agent", "Cinnamon")
+        .query(&[("scope", SCOPE), ("expiry", EXPIRY)]);
+
+    log::info!("Request to send: {:#?}", request);
+
+    let response = request.send().unwrap();
+
+    log::info!("Response code: {}", response.status());
+
+    response.text().unwrap()
+}
+
+/// Exchanges a request token for an access token, corresponding to Stage 3.
+///
+/// Using the `consumer_key`, `consumer_secret`, `oauth_token` and `oauth_verifier`, signs a
+/// request to the SSO service and requests for the request token received previously to become an
+/// access token. This requires the client secret from Stage 1 as well.
+fn exchange_request_for_access(
+    consumer_key: &str,
+    consumer_secret: &str,
+    oauth_token: &str,
+    oauth_verifier: &str,
+) -> String {
+    let secret = &std::fs::read_to_string("secret.txt").unwrap();
+    let token = oauth::Credentials::new(oauth_token, secret);
+
+    let credentials = Credentials::new(consumer_key, consumer_secret);
+    let auth = Builder::<_, _>::new(credentials, oauth::HmacSha1)
+        .verifier(oauth_verifier)
+        .token(Some(token))
+        .post(&ACCESS_TOKEN_URL, &());
+
+    log::info!("Authorization header: {}", auth);
+
+    let client = Client::new();
+    let request = client
+        .post(ACCESS_TOKEN_URL)
+        .header("Authorization", auth)
+        .header("User-Agent", "Cinnamon");
+
+    log::info!("Request to send: {:#?}", request);
+
+    let response = request.send().unwrap();
+    let status = response.status();
+
+    log::info!("Response code: {}", status);
+
+    response.text().unwrap()
+}
+
+/// Builds the web server routing and returns it ready to be launched.
+pub fn build_rocket() -> rocket::Rocket {
+    pretty_env_logger::init();
+    dotenv().unwrap();
+
+    rocket::ignite().mount("/", routes![index, authorised])
+}
